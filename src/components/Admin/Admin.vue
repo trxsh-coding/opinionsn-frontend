0eafd19280df7e7f2c5fb65807b521b6dfc0c371
faraks01 <faraks01@gmail.com>
2019-05-06 11:35:29 +0300

    Начал верстать admin page

--- src/components/Admin/Admin.vue
+++ src/components/Admin/Admin.vue
@@ -1,40 +1,95 @@
 <template>
-    <div class="admin-wrapper">
-        <router-link to="/admin/catalog">
+	<div class="admin-wrapper relative height-100">
+		<!-- <router-link to="/admin/catalog">
             <el-button type="primary" size="small" round>
                 Каталог
             </el-button>
         </router-link>
-        <router-view/>
+		<router-view/>-->
 
-    </div>
+		<el-container style="height: 100%; border: 1px solid #eee">
+			<el-aside width="250px" style="background-color: rgb(238, 241, 246)">
+				<el-menu>
+					<el-submenu index="1">
+						<template slot="title">
+							<i class="el-icon-message"></i>API
+						</template>
+						<el-menu-item index="1-1">Добавить переводы</el-menu-item>
+					</el-submenu>
+					<el-submenu index="2">
+						<template slot="title">
+							<i class="el-icon-menu"></i>Пользователи
+						</template>
+						<el-menu-item index="2-1">Добавить</el-menu-item>
+						<el-menu-item index="2-2">Удалить</el-menu-item>
+						<el-menu-item index="2-3">Редактировать</el-menu-item>
+					</el-submenu>
+					<el-submenu index="3">
+						<template slot="title">
+							<i class="el-icon-setting"></i>Опросы
+						</template>
+						<el-menu-item index="3-1">Добавить</el-menu-item>
+						<el-menu-item index="3-2">Удалить</el-menu-item>
+						<el-menu-item index="3-3">Редактировать</el-menu-item>
+					</el-submenu>
+				</el-menu>
+			</el-aside>
+
+			<el-container>
+				<el-header style="text-align: right; font-size: 12px">
+					<el-dropdown>
+						<i class="el-icon-setting" style="margin-right: 15px"></i>
+						<el-dropdown-menu slot="dropdown">
+							<el-dropdown-item>View</el-dropdown-item>
+							<el-dropdown-item>Add</el-dropdown-item>
+							<el-dropdown-item>Delete</el-dropdown-item>
+						</el-dropdown-menu>
+					</el-dropdown>
+					<span>Tom</span>
+				</el-header>
+
+				<el-main>
+					<el-table :data="tableData">
+						<el-table-column prop="date" label="Date" width="140"></el-table-column>
+						<el-table-column prop="name" label="Name" width="120"></el-table-column>
+						<el-table-column prop="address" label="Address"></el-table-column>
+					</el-table>
+				</el-main>
+			</el-container>
+		</el-container>
+	</div>
 </template>
 
 <script>
-    import Catalog from './Catalog.vue'
-    export default {
-        name: "Admin",
-        data(){
-            return {
-
-            }
-        },
-        components: {
-            Catalog
-
-        }
-
-    }
+	import Catalog from "./Catalog.vue";
+	export default {
+		name: "Admin",
+		data() {
+			return {
+			};
+		},
+		components: {
+			Catalog
+		}
+	};
 </script>
 
 <style lang="scss">
+	// .el-button--primary {
+	// 	background: #4b97b4;
+	// 	border-color: inherit;
+	// }
+	// .el-button {
+	// 	padding: 10px 20px;
+	// }
 
-    .el-button--primary {
-        background:#4B97B4;
-        border-color: inherit;
-    }
-    .el-button {
-        padding: 10px 20px;
-    }
+	.el-header {
+		background-color: #b3c0d1;
+		color: #333;
+		line-height: 60px;
+	}
 
+	.el-aside {
+		color: #333;
+	}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -27,222 +27,221 @@ import CatalogFeed from '../components/CatalogFeed/Feed';
 import feedBack from '../components/feedback/feedbackForm';
 import user from '../components/user/Page';
 import sidebar from '../components/mobile/sidebar';
-import {nprogress} from '../main.js'
+import { nprogress } from '../main.js'
 import notificationPage from '../components/notifications/notificationPage'
 Vue.use(Router);
 
 export const index = new Router({
-  base: '/',
-  mode: 'history',
-  scrollBehavior (to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return { x: 0, y: 0 }
-    }
-  },
-  routes: [
+	base: '/',
+	mode: 'history',
+	scrollBehavior(to, from, savedPosition) {
+		if (savedPosition) {
+			return savedPosition
+		} else {
+			return { x: 0, y: 0 }
+		}
+	},
+	routes: [
 
-    {
-      path: '/login',
-      component: Login,
-      name:'login',
-    },
-    {
-          path: '/registration',
-          name: 'registration',
-          component: Registration,
-    },
-    {
-          path: '/sign',
-          name: 'sign',
-          component: Sign,
-    },
-    {
-      path: '/restore',
-      name: 'restore',
-      component: Restore,
-    },
-    {
-      path: '/token',
-      name: 'token',
-      component: Token,
-      children: [
-        {
-          path: 'password',
-          name: 'password',
-          component: Password
-        },
-      ]
-    },
-    {
-      path: '/',
-      name: 'opinion',
-      component: Main,
-      redirect:
-          {
-            name: 'pollFeed'
-          },
-      children: [
-        {
-          path: '/user/:id',
-          name:'user',
-          component: user,
-          props: true
-        },
-        {
-          path: 'add',
-          name: 'add',
-          component: CreateQuiz,
-          children: [
-            {
-              path: 'poll',
-              name: 'poll',
-              component: PollCreate
-            },
-            {
-              path: 'prediction',
-              name: 'prediction',
-              component: CreatePoll
-            },
+		{
+			path: '/login',
+			component: Login,
+			name: 'login',
+		},
+		{
+			path: '/registration',
+			name: 'registration',
+			component: Registration,
+		},
+		{
+			path: '/sign',
+			name: 'sign',
+			component: Sign,
+		},
+		{
+			path: '/restore',
+			name: 'restore',
+			component: Restore,
+		},
+		{
+			path: '/token',
+			name: 'token',
+			component: Token,
+			children: [
+				{
+					path: 'password',
+					name: 'password',
+					component: Password
+				},
+			]
+		},
+		{
+			path: '/admin',
+			name: 'admin',
+			component: Admin,
+			children: [
+				{
+					path: 'catalog',
+					name: 'catalog',
+					component: Catalog
+				},
+			]
+		},
+		{
+			path: '/',
+			name: 'opinion',
+			component: Main,
+			redirect:
+			{
+				name: 'pollFeed'
+			},
+			children: [
+				{
+					path: '/user/:id',
+					name: 'user',
+					component: user,
+					props: true
+				},
+				{
+					path: 'add',
+					name: 'add',
+					component: CreateQuiz,
+					children: [
+						{
+							path: 'poll',
+							name: 'poll',
+							component: PollCreate
+						},
+						{
+							path: 'prediction',
+							name: 'prediction',
+							component: CreatePoll
+						},
 
-          ]
-        },
+					]
+				},
 
-        {
-          path: 'admin',
-          name: 'admin',
-          component: Admin,
-          children: [
-            {
-              path: 'catalog',
-              name: 'catalog',
-              component: Catalog
-            },
+				{
+					path: 'voteFeed',
+					name: 'voteFeed',
+					component: VoteFeed,
+				},
+				{
+					path: 'feedBack',
+					name: 'feedBack',
+					component: feedBack,
+				},
+				{
+					path: 'notifications',
+					name: 'notifications',
+					component: notificationPage,
+				},
+				{
+					path: 'search',
+					name: 'search',
+					component: search,
+				},
+				{
+					path: 'menu',
+					name: 'menu',
+					component: sidebar,
+				},
+				{
+					path: 'follows',
+					name: 'follows',
+					component: follows,
+					redirect:
+					{
+						name: 'followers'
+					},
+					children: [
+						{
+							path: 'followings/:id',
+							name: 'followings',
+							component: followings,
+							props: { isFollowing: true }
+						},
+						{
+							path: 'followers/:id',
+							name: 'followers',
+							component: followers,
+							props: { isFollowing: false }
+						},
 
-          ]
-        },
-        {
-          path: 'voteFeed',
-          name: 'voteFeed',
-          component: VoteFeed,
-        },
-        {
-          path: 'feedBack',
-          name: 'feedBack',
-          component: feedBack,
-        },
-        {
-          path: 'notifications',
-          name: 'notifications',
-          component: notificationPage,
-        },
-        {
-          path: 'search',
-          name: 'search',
-          component: search,
-        },
-        {
-          path: 'menu',
-          name: 'menu',
-          component: sidebar,
-        },
-        {
-          path: 'follows',
-          name: 'follows',
-          component: follows,
-          redirect:
-              {
-                name: 'followers'
-              },
-          children: [
-            {
-              path: 'followings/:id',
-              name: 'followings',
-              component: followings,
-              props:{isFollowing : true}
-            },
-            {
-              path: 'followers/:id',
-              name: 'followers',
-              component: followers,
-              props:{isFollowing : false}
-            },
+					]
+				},
+				{
+					path: 'pollFeed',
+					name: 'pollFeed',
+					component: PollFeed,
+					props: { feed: true }
 
-          ]
-        },
-        {
-          path: 'pollFeed',
-          name: 'pollFeed',
-          component: PollFeed,
-          props:{feed: true}
-
-        },
-        {
-          path: 'catalogList',
-          name: 'catalogList',
-          component: CatalogList,
-        },
-        {
-          path: 'bookmarkFeed',
-          name: 'bookmarkFeed',
-          component: bookmarkFeed,
-        },
-        {
-          path: 'catalogFeed/:id',
-          name: 'catalogFeed',
-          component: CatalogFeed,
-        },
-        {
-          path: 'singlePoll/:id',
-          name: 'singlePoll',
-          component: Poll,
-          props:{feed: false}
-        },
-      ]
-    },
-  ]
+				},
+				{
+					path: 'catalogList',
+					name: 'catalogList',
+					component: CatalogList,
+				},
+				{
+					path: 'bookmarkFeed',
+					name: 'bookmarkFeed',
+					component: bookmarkFeed,
+				},
+				{
+					path: 'catalogFeed/:id',
+					name: 'catalogFeed',
+					component: CatalogFeed,
+				},
+				{
+					path: 'singlePoll/:id',
+					name: 'singlePoll',
+					component: Poll,
+					props: { feed: false }
+				},
+			]
+		},
+	]
 })
 
 
 index.beforeResolve((to, from, next) => {
-  if(to.path) {
-    nprogress.start()
-  }
-  next()
+	if (to.path) {
+		nprogress.start()
+	}
+	next()
 });
 
-index.afterEach(()=> {
-  nprogress.done()
+index.afterEach(() => {
+	nprogress.done()
 });
 index.beforeEach((to, from, next) => {
-  const publicPages = ['/login'];
-  next();
+	const publicPages = ['/login'];
+	next();
 });
 
-index.beforeEach((to, from, next) =>  {
-  if (to.path === '/sign' || to.path === '/registration' || to.path === '/login' || to.path === '/restore' || to.name === `token` || to.path === '/pollFeed' || to.name === 'singlePoll' || to.name === 'feedBack') {
-    next({
-      query: { redirect: to.fullPath }
-    });
-  } else {
-    axios.get('/api/rest/getUserStatus')
+index.beforeEach((to, from, next) => {
+	if (to.path === '/sign' || to.path === '/registration' || to.path === '/login' || to.path === '/restore' || to.name === `token` || to.path === '/pollFeed' || to.name === 'singlePoll' || to.name === 'feedBack') {
+		next({
+			query: { redirect: to.fullPath }
+		});
+	} else {
+		axios.get('/api/rest/getUserStatus')
 
-        .then(response => {
+			.then(response => {
 
-          if(response.status === 200)  {
+				if (response.status === 200) {
 
 
-            next();
+					next();
 
-          }
-        })
-        .catch(error => {
+				}
+			})
+			.catch(error => {
 
-          next('/login')
+				next('/login')
 
-        })
-  }
+			})
+	}
 });
 
 export default index
