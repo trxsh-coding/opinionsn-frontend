5119ea95e945625f8917848b279328df95c50ff8
faraks01 <faraks01@gmail.com>
2019-05-13 16:36:54 +0300

    ошибочный реверт

--- src/components/LeftBarSide.vue
+++ src/components/LeftBarSide.vue
@@ -1,5 +1,6 @@
 <template lang="html">
     <div id="left-aside">
+<<<<<<< HEAD
     	<profile-card class="hidden-xs-only" />
     	<side-bar />
     	<user-statistic  v-if="user" :id="main_user.id" :limit="1" class="hidden-xs-only"/>
@@ -9,85 +10,99 @@
 			<router-link class="pointer" tag="span" to="/feedback"><lang-string :title="'feedback'"/></router-link>
         </div>
       	<!--<guest-locale  class="hidden-xs-only" />-->
+=======
+      <profile-card class="hidden-xs-only" />
+        <side-bar />
+        <user-statistic  v-if="user" :id="main_user.id" :limit="1" class="hidden-xs-only"/>
+        <language-change  class="hidden-xs-only" />
+        <!--<guest-locale  class="hidden-xs-only" />-->
+>>>>>>> parent of c7467782... Изменил feedback форму
     </div>
 </template>
 
 <script>
-	import { mapState } from "vuex";
-	import ProfileCard from "./LeftSideProfile.vue";
-	import SideBar from "./SideBar.vue";
-	import Statistic from "./Statistic.vue";
-	import languageChange from "./languageChange.vue";
-	import guestLocale from "./guestLocale.vue";
-	import langString from "./langString.vue";
-	import userStatistic from "./user/Event/modules/userStatistic";
+import { mapState } from 'vuex';
+import ProfileCard from './LeftSideProfile.vue'
+import SideBar from './SideBar.vue'
+import Statistic from './Statistic.vue'
+import languageChange from './languageChange.vue'
+import guestLocale from './guestLocale.vue'
+import langString from './langString.vue'
+import userStatistic from './user/Event/modules/userStatistic'
 
-	export default {
-		props: ["id"],
-		data() {
-			return {};
-		},
-		computed: {
-			...mapState("globalStore", {
-				userMap: ({ users }) => users
-			}),
+export default {
+  props:['id'],
+  data () {
+    return {
 
-			...mapState("userPage", {
-				state: s => s,
-				main_user_id: s => s.main_user_id
-			}),
+    }
+  },
+  computed: {
 
-			loop: function() {
-				return "v-for='(item, key) in items.slice(0,2)'";
-			},
+      ...mapState('globalStore', {
 
-			main_user: function() {
-				let { state, userMap } = this;
+          userMap: ({users}) => users
 
-				return userMap[state.main_user_id];
-			}
-		},
-		components: {
-			SideBar,
-			ProfileCard,
-			Statistic,
-			languageChange,
-			langString,
-			userStatistic,
-			guestLocale
-		}
-	};
+      }),
+
+      ...mapState('userPage', {
+          state: s => s,
+          main_user_id: s => s.main_user_id
+      }),
+
+
+      loop:function() {
+
+        return "v-for='(item, key) in items.slice(0,2)'"
+
+      },
+
+      main_user:function () {
+
+          let {state,userMap} = this;
+
+          return userMap[state.main_user_id]
+
+      },
+  },
+  components: {
+    SideBar,
+    ProfileCard,
+    Statistic,
+    languageChange,
+    langString,
+    userStatistic,
+    guestLocale
+
+  }
+}
+
 </script>
 
 <style lang="scss">
-	#left-aside {
-		width: 240px !important;
-		position: fixed;
 
-		.user-statistic {
-			justify-content: center;
-			.circle-chart {
-				padding: 12px 30px;
-			}
-		}
+  #left-aside {
+      width: 240px !important;
+      position: fixed;
 
-		.el-aside {
-			overflow-x: hidden;
-		}
-		.circle {
-			display: inline-block;
-		}
+      .user-statistic {
 
-		.copyright {
-			padding: 0 10px;
-			span {
-				font-family: Roboto;
-				font-style: normal;
-				font-weight: normal;
-				font-size: 11px;
-				letter-spacing: -0.3px;
-				color: rgba(56, 56, 56, 0.7);
-			}
-		}
-	}
+          justify-content: center;
+          .circle-chart {
+
+          padding: 12px 30px;
+
+          }
+
+      }
+
+
+      .el-aside {
+          overflow-x: hidden;
+      }
+      .circle {
+          display: inline-block;
+      }
+
+  }
 </style>
--- src/components/feedback/feedbackForm.vue
+++ src/components/feedback/feedbackForm.vue
@@ -1,105 +1,119 @@
 <template>
-	<div class="feedback-form">
-		<div class="feedback-header">
-			<span>
-				Вы находитесь на тестовой версии первой crowd wisdom платформы на основе технологии Blockchain.
-				<br>
-				<br>Opinion — это социальная сеть, где пользователи участвуют в опросах, делятся мнениями, предсказывают исход событий и получают вознаграждения за точные прогнозы.
-				<br>
-				<br>Мы будем Вам благодарны за оставленный отзыв по работе системы или предложение по ее улучшению.
-				Отзыв можно оставить, заполнив форму или отправив нам сообщение в социальных сетях.
-			</span>
-		</div>
-		<div class="form-section mt-15">
-			<el-form v-model="feedbackForm" ref="feedbackForm">
-				<el-form-item>
-					<el-input placeholder="E-mail" v-model="feedbackForm.email"/>
-				</el-form-item>
+  <div class="feedback-form">
+    <div class="feedback-header">
+      <span>
+        Вы находитесь на тестовой версии первой crowd wisdom платформы на основе технологии Blockchain.
+        <br>
+        <br>Opinion — это социальная сеть, где пользователи участвуют в опросах, делятся мнениями, предсказывают исход событий и получают вознаграждения за точные прогнозы.
+        <br>
+        <br>Мы будем Вам благодарны за оставленный отзыв по работе системы или предложение по ее улучшению.
+        Отзыв можно оставить, заполнив форму или отправив нам сообщение в социальных сетях.
+      </span>
+    </div>
+    <div class="form-section mt-15">
+      <el-form v-model="feedbackForm" ref="feedbackForm">
+        <el-form-item>
+          <el-input placeholder="E-mail" v-model="feedbackForm.email"/>
+        </el-form-item>
 
-				<el-form-item>
-					<el-input
-						class="message"
-						placeholder="Текст сообщения"
-						type="textarea"
-						v-model="feedbackForm.description"
-					/>
-				</el-form-item>
+        <el-form-item>
+          <el-input
+            class="message"
+            placeholder="Текст сообщения"
+            type="textarea"
+            v-model="feedbackForm.description"
+          />
+        </el-form-item>
 
-				<div class="button-section flex-space-start">
-					<el-button class="primary-btn" @click.prevent="submitForm(feedbackForm)" size="small">Отправить</el-button>
-					<el-upload
-						class="upload-demo"
-						multiple
-						:before-remove="beforeRemove"
-						:auto-upload="false"
-						:limit="3"
-						:on-change="handlePreview"
-						ref="upload"
-					>
-						<el-button type="primary" size="small">Прикрепить изображения</el-button>
-					</el-upload>
-				</div>
-			</el-form>
-		</div>
+        <div class="button-section flex-space-start mt-40">
+          <el-button class="primary-btn" @click.prevent="submitForm(feedbackForm)">Отправить</el-button>
+          <el-upload
+            class="upload-demo"
+            :auto-upload="false"
+            multiple
+            :limit="3"
+            :on-change="handlePreview"
+            ref="upload"
+          >
+          <!-- <el-upload
+            class="upload-demo"
+            :before-remove="beforeRemove"
+            :auto-upload="false"
+            multiple
+            :limit="3"
+            :on-change="handlePreview"
+            ref="upload"
+          > -->
+            <el-button type="primary">Прикрепить изображения</el-button>
+            <!-- <div slot="tip" class="el-upload__tip">jpg/png files with a size less than 500kb</div> -->
+          </el-upload>
+        </div>
+      </el-form>
+    </div>
 
-		<ul class="soc-media mt-5">
-			<li>
-				<a href="https://www.facebook.com/opinionsncom/" target="_blank" rel="noopener noreferrer">
-					<div
-						class="picture-30x30"
-						:style="{ 'background-image': 'url(' + require('../images/facebook.png') + ')' } "
-					/>
-				</a>
-			</li>
-			<li>
-				<a href="https://twitter.com/opinionsn_com" target="_blank" rel="noopener noreferrer">
-					<div
-						class="picture-30x30"
-						:style="{ 'background-image': 'url(' + require('../images/twitter.png') + ')' } "
-					/>
-				</a>
-			</li>
-			<li>
-				<a href="https://www.instagram.com/opinionsn_com/" target="_blank" rel="noopener noreferrer">
-					<div
-						class="picture-30x30"
-						:style="{ 'background-image': 'url(' + require('../images/instagram.png') + ')' } "
-					/>
-				</a>
-			</li>
-			<li>
-				<a href="https://t.me/opinion_social_network" target="_blank" rel="noopener noreferrer">
-					<div
-						class="picture-30x30"
-						:style="{ 'background-image': 'url(' + require('../images/telegram.png') + ')' } "
-					/>
-				</a>
-			</li>
-			<li>
-				<a href="https://vk.com/opinion_social_network" target="_blank" rel="noopener noreferrer">
-					<div
-						class="picture-30x30"
-						:style="{ 'background-image': 'url(' + require('../images/vk.png') + ')' } "
-					/>
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://www.youtube.com/channel/UClapvCFqBLMO8vjAPv4KohQ?view_as=subscriber"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<div
-						class="picture-30x30"
-						:style="{ 'background-image': 'url(' + require('../images/youtube.png') + ')' } "
-					/>
-				</a>
-			</li>
-		</ul>
-	</div>
+    <ul class="soc-media">
+      <li>
+        <a href="https://www.facebook.com/opinionsncom/" target="_blank" rel="noopener noreferrer">
+          <div
+            class="picture-30x30"
+            :style="{ 'background-image': 'url(' + require('../images/facebook.png') + ')' } "
+          />
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/opinionsn_com" target="_blank" rel="noopener noreferrer">
+          <div
+            class="picture-30x30"
+            :style="{ 'background-image': 'url(' + require('../images/twitter.png') + ')' } "
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.instagram.com/opinionsn_com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div
+            class="picture-30x30"
+            :style="{ 'background-image': 'url(' + require('../images/instagram.png') + ')' } "
+          />
+        </a>
+      </li>
+      <li>
+        <a href="https://t.me/opinion_social_network" target="_blank" rel="noopener noreferrer">
+          <div
+            class="picture-30x30"
+            :style="{ 'background-image': 'url(' + require('../images/telegram.png') + ')' } "
+          />
+        </a>
+      </li>
+      <li>
+        <a href="https://vk.com/opinion_social_network" target="_blank" rel="noopener noreferrer">
+          <div
+            class="picture-30x30"
+            :style="{ 'background-image': 'url(' + require('../images/vk.png') + ')' } "
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.youtube.com/channel/UClapvCFqBLMO8vjAPv4KohQ?view_as=subscriber"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div
+            class="picture-30x30"
+            :style="{ 'background-image': 'url(' + require('../images/youtube.png') + ')' } "
+          />
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+<<<<<<< HEAD
 	import axios from "axios";
 	export default {
 		name: "feedbackForm",
@@ -268,4 +282,127 @@
 			}
 		}
 	}
+=======
+import axios from "axios";
+export default {
+  name: "feedbackForm",
+  data() {
+    return {
+      pictures: [],
+      feedbackForm: {
+        email: "",
+        description: ""
+      }
+    };
+  },
+  methods: {
+    handlePreview(file) {
+      this.pictures.push(file);
+    },
+    submitForm(formName) {
+      var bodyFormData = new FormData();
+      let { pictures } = this;
+      const form = new Blob([JSON.stringify(formName)], {
+        type: "application/json"
+      });
+      bodyFormData.append("form", form);
+      for (let pic of pictures) {
+        bodyFormData.append("files[]", pic.raw);
+      }
+      const config = {
+        headers: {
+          "content-type": "multipart/mixed"
+        }
+      };
+      axios.post("/api/feedback", bodyFormData, config).then(
+        function(response) {
+          if (response.status === 200) {
+            this.loading = false;
+            this.$router.push({ name: "pollFeed" });
+          }
+        }.bind(this)
+      );
+    },
+    // beforeRemove(file, fileList) {
+    //   return this.$confirm(`Cancel the transfert of ${file.name} ?`);
+    // }
+  }
+};
+</script>
+
+<style lang="scss">
+.feedback-form {
+  padding: 20px;
+  border-radius: 12px;
+  background: #ffffff;
+  text-align: center;
+
+  span {
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 16px;
+    color: #152d3a;
+    padding-right: 6px;
+  }
+  .form-section {
+    .el-input__inner,
+    .el-textarea__inner {
+      border-radius: 12px !important;
+    }
+
+    .el-textarea__inner {
+      height: 160px;
+      resize: none;
+    }
+
+    .button-section {
+      min-height: 135px;
+
+      span {
+        color: #ffffff;
+      }
+
+      .el-button {
+        border-radius: 12px !important;
+      }
+    }
+
+    @media only screen and (max-device-width: 700px) {
+      .button-section {
+        min-height: 195px;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+
+        .upload-demo {
+          margin-top: 14px;
+        }
+      }
+    }
+  }
+
+  .soc-media {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    width: max-content;
+
+    li {
+      list-style-type: none;
+      margin-right: 15px;
+
+      &:last-child {
+        margin: 0;
+      }
+    }
+  }
+
+  @media only screen and (max-device-width: 700px) {
+    .soc-media {
+      margin: 0 auto;
+    }
+  }
+}
+>>>>>>> parent of c7467782... Изменил feedback форму
 </style>
