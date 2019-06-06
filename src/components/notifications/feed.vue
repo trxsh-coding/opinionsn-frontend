9401147e67a97ee8c538c14f997bf342b90a3ef7
faraks01 <faraks01@gmail.com>
2019-06-06 16:11:59 +0300

    Фиксы верхних уведомлений

--- src/components/notifications/feed.vue
+++ src/components/notifications/feed.vue
@@ -1,127 +1,156 @@
 <template>
-  <el-dropdown trigger="click" @click.native="clearCounter" >
-    <el-badge :value=" counter > 9 ? '9+': counter " :hidden="hidden_badge" class="item pointer" >
-      <icon-base class="" width="17" height="22" viewBox="0 0 17 22" icon-name="bell" >
-        <icon-bell/>
-      </icon-base>
-    </el-badge>
-    <el-dropdown-menu class="notification-dropdown flex-column" slot="dropdown" v-if="messages" v-loading="!loading">
-    <!-- <el-dropdown-menu class="notification-dropdown flex-column" slot="dropdown" v-if="false"> -->
-      <feed-block
-        :notification="notification"
-        v-for="(notification, index) in messages"
-        :key="index"
-      />
-    </el-dropdown-menu>
-    <el-dropdown-menu class="notification-dropdown flex-column noMessages" slot="dropdown"  v-else>
-      <div class="notification-feed-placeholder v-center">
+	<el-dropdown trigger="click" @click.native="clearCounter">
+		<el-badge :value=" counter > 9 ? '9+': counter " :hidden="hidden_badge" class="item pointer">
+			<icon-base class="" width="17" height="22" viewBox="0 0 17 22" icon-name="bell">
+				<icon-bell/>
+			</icon-base>
+		</el-badge>
+		<el-dropdown-menu class="notification-dropdown flex-column" slot="dropdown" v-if="Object.keys(messages).length !== 0"
+						  v-loading="!loading">
+			<!-- <el-dropdown-menu class="notification-dropdown flex-column" slot="dropdown" v-if="false"> -->
+			<feed-block
+				:notification="notification"
+				v-for="(notification, index) in messages"
+				:key="index"
+			/>
+		</el-dropdown-menu>
+		<el-dropdown-menu class="notification-dropdown flex-column noMessages" slot="dropdown" v-else>
+			<div class="notification-feed-placeholder v-center">
         <span>
           Новые уведомления отсуствуют
         </span>
-      </div>
-    </el-dropdown-menu>
-  </el-dropdown>
+			</div>
+		</el-dropdown-menu>
+	</el-dropdown>
 </template>
 
 <script>
-import feedBlock from "./feedBlock";
-import IconBase from "../icons/IconBase.vue";
-import IconBell from "../icons/IconBell.vue";
-import { mapState } from "vuex";
-import axios from 'axios'
-export default {
-  name: "feed",
-  components: {
-    feedBlock,
-    IconBase,
-    IconBell
-  },
-  data() {
-    return {
+	import feedBlock from "./feedBlock";
+	import IconBase from "../icons/IconBase.vue";
+	import IconBell from "../icons/IconBell.vue";
+	import {mapState} from "vuex";
+	import axios from 'axios'
 
-      hidden:false
+	export default {
+		name: "feed",
+		components: {
+			feedBlock,
+			IconBase,
+			IconBell
+		},
+		data() {
+			return {
 
-    };
-  },
-  methods: {
+				hidden: false
 
-    clearCounter(){
-      this.$store.dispatch('notificationPage/readInitialNotifications')
+			};
+		},
+		methods: {
 
-    }
+			clearCounter() {
+				this.$store.dispatch('notificationPage/readInitialNotifications')
 
-  },
-  computed: {
-    ...mapState("notificationStore", {
-      messages: s => s.messages
-    }),
+			}
 
-    ...mapState("notificationPage", {
-      counter: s => s.counter,
-      loading: s => s.loading
+		},
+		computed: {
+			...mapState("notificationStore", {
+				messages: s => s.messages
+			}),
 
-    }),
+			...mapState("notificationPage", {
+				counter: s => s.counter,
+				loading: s => s.loading
 
-    hidden_badge:function () {
+			}),
 
-      if(this.counter == 0) {
+			hidden_badge: function () {
 
-        return this.hidden = true;
+				if (this.counter == 0) {
 
-      }
+					return this.hidden = true;
 
-    }
+				}
 
-  },
-  created(){
-    axios.get(`${process.env.VUE_APP_NOTIFICATION_API}/notification/unReadCount`)
-            .then(response => {
-                this.$store.commit('notificationPage/setNotificationsCount', response.data)
+			}
 
-            })
+		},
+		created() {
+			console.log('MESSAGES', this.messages);
+
+			axios.get(`${process.env.VUE_APP_NOTIFICATION_API}/notification/unReadCount`)
+				.then(response => {
+					this.$store.commit('notificationPage/setNotificationsCount', response.data)
+
+				})
 
 
-  }
-};
+		}
+	};
 </script>
 
 <style lang="scss" scoped>
-.hidden {
-  display: none;
-}
+	.hidden {
+		display: none;
+	}
 
-.notification-feed-block:nth-last-child {
+	.notification-feed-block:nth-last-child {
 
-  border: none !important;
+		border: none !important;
 
-}
+	}
 
-.notification-dropdown {
-  @media only screen and (max-device-width : 700px) {
-    width: 90%;
-  }
-  width: 488px;
-  padding: 12px;
-  border-radius: 12px;
-	max-height: 505px;
-	margin-bottom: 0;
-  &::-webkit-scrollbar {
-    display: none;
-  }
+	.notification-dropdown {
+		@media only screen and (max-device-width: 700px) {
+			width: 90%;
+		}
+		width: 488px;
+		padding: 12px;
+		border-radius: 12px;
+		max-height: 505px;
+		margin-bottom: 0;
 
-  &.noMessages {
-    // border-color: #e2e2e2;
-    // background-color: #f3f3f3;
-    color: #69777F;
-    font-family: Roboto;
-    font-style: normal;
-  }
+		overflow-x: hidden;
 
-  .notification-feed-placeholder {
-    width: 100%;
-    background-color: transparent;
-  }
-}
+		scrollbar-width: thin;
+
+		&::-webkit-scrollbar {
+			width: 14px;
+			height: 18px;
+		}
+
+		&::-webkit-scrollbar-thumb {
+			height: 6px;
+			border: 4px solid rgba(0, 0, 0, 0);
+			background-clip: padding-box;
+			-webkit-border-radius: 7px;
+			background-color: rgba(0, 0, 0, 0.15);
+			-webkit-box-shadow: inset -1px -1px 0px rgba(0, 0, 0, 0.05), inset 1px 1px 0px rgba(0, 0, 0, 0.05);
+		}
+
+		&::-webkit-scrollbar-button {
+			width: 0;
+			height: 0;
+			display: none;
+		}
+
+		&::-webkit-scrollbar-corner {
+			background-color: transparent;
+		}
+
+		&.noMessages {
+			// border-color: #e2e2e2;
+			// background-color: #f3f3f3;
+			color: #69777F;
+			font-family: Roboto;
+			font-style: normal;
+		}
+
+		.notification-feed-placeholder {
+			width: 100%;
+			background-color: transparent;
+		}
+	}
 
 
 </style>
