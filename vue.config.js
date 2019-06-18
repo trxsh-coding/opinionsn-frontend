ab2f4dc0120deb23b904c846ef5af1da0eac091c
Orest <k.orest@opinionsn.com>
2019-06-18 17:41:12 +0300

    Добавлено pwa

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,4 @@ module.exports = {
 	indexPath: "index.html",
 	outputDir: "www",
 	assetsDir: '',
-	pwa: {
-		workboxPluginMode: 'InjectManifest',
-		workboxOptions: {
-			swSrc: 'src/service-worker.js'
-		},
-		themeColor: '#1da025'
-	},
 }
