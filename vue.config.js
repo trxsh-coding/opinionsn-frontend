aa5f571edecf0b2d6612934f5612a0640695450a
faraks01 <faraks01@gmail.com>
2019-06-19 14:16:25 +0300

    == прописал прокси на продакшен

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,28 @@ module.exports = {
 	baseUrl: '/',
 	indexPath: "index.html",
 	outputDir: "www",
-	assetsDir: '',
+	devServer: {
+		https:true,
+		proxy: {
+			'/api': {
+				target: 'https://opinionsn.com/api',
+				pathRewrite: {'^/api': '/'},
+				changeOrigin: true,
+				secure: false
+			},
+			'/messages': {
+				target: 'https://opinionsn.com/messages',
+				pathRewrite: {'^/messages': '/'},
+				changeOrigin: true,
+				secure: false
+			},
+			'/assets': {
+				target: 'https://opinionsn.com/assets',
+				// pathRewrite: {'^/messages': '/'},
+				changeOrigin: true,
+				secure: false
+			}
+
+		}
+	}
 }
