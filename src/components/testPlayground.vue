f944a37c7137ff586c00b2b2da0b9225bba1161a
faraks01 <faraks01@gmail.com>
2019-06-24 14:14:11 +0300

    == добавил в Admin компонент для тестирования компонентов

---
+++ src/components/testPlayground.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="wrapper-box">
+		<picture-reusable :img="require('../assets/logo.png')" :size="60" textLayout="left" :counter="10" bor-rad="50%">
+			<template #title>
+				qwerty
+			</template>
+			<template #description>
+				qwertydsdfs
+			</template>
+		</picture-reusable>
+	</div>
+</template>
+
+<script>
+    import PictureReusable from "./reusableСomponents/PictureReusable";
+
+	export default {
+        name: "testPlayground",
+		components: {
+			PictureReusable
+		},
+		mounted() {
+			console.log(require('../assets/logo.png'));
+			console.log(this.picture);
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.wrapper-box {
+		width: 100%;
+	}
+</style>
