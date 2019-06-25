c1592ae68837eca76fdec8803c42f159c23912a5
faraks01 <faraks01@gmail.com>
2019-06-25 13:24:41 +0300

    == начал работу над DropdownListReusable компонентом

---
+++ src/components/reusableСomponents/DropdownListReusable.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="dropdown-list-reusable">
+
+		<button class="toggle-btn" @click="show = !show">
+			<slot>Кино <==></slot>
+<!--		TODO: вставить иконку svg	-->
+		</button>
+
+		<ul class="dropdown-list" :style="listVisibility">
+			<slot name="items"></slot>
+		</ul>
+	</div>
+</template>
+
+<script>
+    export default {
+        name: "DropdownListReusable",
+		data() {
+			return {
+				show: false
+			}
+		},
+		computed: {
+			listVisibility() {
+				return this.show ? { visibility: 'visible', opacity: '1' } : { visibility: 'hidden', opacity: '0' };
+			}
+		},
+	//	TODO: сделать эмит для this.show
+    }
+</script>
+
+<style lang="scss" scoped>
+
+	.dropdown-list-reusable {
+
+		margin-left: 100px;
+
+		position: relative;
+		width: fit-content;
+
+		.toggle-btn {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			cursor: pointer;
+			padding: 1px 0;
+
+			font-family: Roboto;
+			font-style: normal;
+			font-weight: 500;
+			font-size: 14px;
+			color: #1A1E22;
+
+			background-color: transparent;
+			/*outline: none;*/
+			/*border: none;*/
+
+		}
+
+		.dropdown-list {
+			position: absolute;
+			z-index: 8000;
+			top: 150%;
+			left: 50%;
+			transform: translateX(-50%);
+
+			background: #FFFFFF;
+			border: 0.3px solid #D8D9DC;
+			box-shadow: 0 0 30px rgba(41, 46, 62, 0.2);
+			border-radius: 6px;
+			padding: 9px;
+
+			transition: 300ms;
+
+			& > * {
+				margin-bottom: 5px;
+
+				&:last-of-type {
+					margin-bottom: 0;
+				}
+			}
+		}
+
+	}
+
+</style>
--- src/components/testPlayground.vue
+++ src/components/testPlayground.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="wrapper-box">
+
 		<option-reusable :percentage="55" :bows="bows" correct :picture="picture" :id="123" @selectOption="selectOption">
 			<template>
 				Да, потому что киберпанк в моде
@@ -9,7 +10,19 @@
 			</template>
 		</option-reusable>
 
-		<picture-reusable :size="60" :img="picture" :counter="6"  ></picture-reusable>
+		<picture-reusable :size="60" :img="picture" :counter="6" bor-rad="50%"></picture-reusable>
+
+		<dropdown-list-reusable>
+
+			<template #items>
+				<li>123123123</li>
+				<li>456456456</li>
+				<li>789789789</li>
+			</template>
+
+
+		</dropdown-list-reusable>
+
 	</div>
 </template>
 
@@ -17,6 +30,7 @@
     import PictureReusable from "./reusableСomponents/PictureReusable";
 	import OptionReusable from "./reusableСomponents/OptionReusable";
 	import BadgeReusable from "./reusableСomponents/BadgeReusable";
+	import DropdownListReusable from "./reusableСomponents/DropdownListReusable";
 
 	export default {
         name: "testPlayground",
@@ -40,6 +54,7 @@
 			}
 		},
 		components: {
+			DropdownListReusable,
 			BadgeReusable,
 			PictureReusable,
 			OptionReusable
