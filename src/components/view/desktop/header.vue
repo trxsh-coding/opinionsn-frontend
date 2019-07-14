c2c653191a8491327840263de6cf36ee3a6f676a
arez123 <faraks01@gmail.com>
2019-07-14 20:28:02 +0300

    == DropdownListReusable добавил параметры на ширину/высоту, сделал слежение за скроллом для подгрузки данных == router фикс роутов == notificationPage сделал подрузку данных по скроллу если это секция (route.name === '/notifications') == Доделал рабочий вариант уведомлений

--- src/components/view/desktop/header.vue
+++ src/components/view/desktop/header.vue
@@ -1,145 +1,201 @@
 <template>
-    <div class="desktop-header mb-17">
-        <div class="nav-container container flex-align-center">
-            <div class="search-block flex-align-center">
-                <icon-base
-                        class="mr-5"
-                        fill="none"
-                        width="23"
-                        height="24"
-                        viewBox="0 0 23 24"
-                        icon-name="text"><icon-logo/>
-                </icon-base>
-                <icon-base
-                        class="mr-27"
-                        fill="none"
-                        width="63"
-                        height="15"
-                        viewBox="0 0 63 15"
-                        icon-name="text"><icon-text-logo/>
-                </icon-base>
-                <input type="text" placeholder="Поиск">
-            </div>
-            <div class="profile-annotation-block flex-align-center">
+	<div class="desktop-header mb-17">
+		<div class="nav-container container flex-align-center">
+			<div class="search-block flex-align-center">
+				<icon-base
+						class="mr-5"
+						fill="none"
+						width="23"
+						height="24"
+						viewBox="0 0 23 24"
+						icon-name="text">
+					<icon-logo/>
+				</icon-base>
+				<icon-base
+						class="mr-27"
+						fill="none"
+						width="63"
+						height="15"
+						viewBox="0 0 63 15"
+						icon-name="text">
+					<icon-text-logo/>
+				</icon-base>
+				<input type="text" placeholder="Поиск">
+			</div>
+			<div class="profile-annotation-block flex-align-center">
 
-                <dropdown-list-reusable width="376">
-                        <icon-base
-                                fill="none"
-                                class="mr-22"
-                                width="22"
-                                height="22"
-                                viewBox="0 0 22 22"
-                                icon-name="notifications"><icon-notifications/>
-                        </icon-base>
-                    <template #items>
-                        <notification-page />
-                    </template>
-                </dropdown-list-reusable>
-                <icon-base
-                        fill="none"
-                        class="mr-25"
-                        width="21"
-                        height="21"
-                        viewBox="0 0 21 21"
-                        icon-name="pocket"><icon-pocket/>
-                </icon-base>
-                <picture-reusable @click="userLink(user.id)" :img="publicPath + user.path_to_avatar" :size="27" bor-rad="50%" />
-                <dropdown-list-reusable >
-                    <template #icon>
-                        <icon-base
-                                fill="none"
-                                class="mr-25"
-                                width="7"
-                                height="4"
-                                viewBox="0 0 7 4"
-                                icon-name="pocket"><icon-dropdown/>
-                        </icon-base>
-                    </template>
-                </dropdown-list-reusable>
-            </div>
-        </div>
-    </div>
+				<dropdown-list-reusable
+						@scrollDifference="setListScrollDifference"
+						list-class="notification-list p-0 pr-7"
+						height="583"
+						width="376">
+					<icon-base
+							fill="none"
+							class="mr-22"
+							width="22"
+							height="22"
+							viewBox="0 0 22 22"
+							icon-name="notifications">
+						<icon-notifications/>
+					</icon-base>
+					<template #items>
+						<notification-page
+								:scrollDifference="listScrollDifference"/>
+					</template>
+				</dropdown-list-reusable>
+				<icon-base
+						fill="none"
+						class="mr-25"
+						width="21"
+						height="21"
+						viewBox="0 0 21 21"
+						icon-name="pocket">
+					<icon-pocket/>
+				</icon-base>
+				<picture-reusable
+						class="pointer"
+						@click.native="userLink(user.id)"
+						:img="publicPath + user.path_to_avatar"
+						:size="27"
+						rounded/>
+				<dropdown-list-reusable>
+					<template #icon>
+						<icon-base
+								fill="none"
+								class="mr-25"
+								width="7"
+								height="4"
+								viewBox="0 0 7 4"
+								icon-name="pocket">
+							<icon-dropdown/>
+						</icon-base>
+					</template>
+				</dropdown-list-reusable>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    import IconBase from "../../icons/IconBase";
-    import IconTextLogo from "../../icons/IconTextLogo";
-    import IconLogo from "../../icons/IconLogo";
-    import IconNotifications from "../../icons/header/IconNotifications";
-    import IconPocket from "../../icons/header/IconPocket";
-    import IconDropdown from "../../icons/header/IconDropdown";
-    import PictureReusable from "../../reusableСomponents/PictureReusable";
-    import DropdownListReusable from "../../reusableСomponents/DropdownListReusable";
-    import NotificationPage from "../../notifications/notificationPage";
+	import IconBase from "../../icons/IconBase";
+	import IconTextLogo from "../../icons/IconTextLogo";
+	import IconLogo from "../../icons/IconLogo";
+	import IconNotifications from "../../icons/header/IconNotifications";
+	import IconPocket from "../../icons/header/IconPocket";
+	import IconDropdown from "../../icons/header/IconDropdown";
+	import PictureReusable from "../../reusableСomponents/PictureReusable";
+	import DropdownListReusable from "../../reusableСomponents/DropdownListReusable";
+	import NotificationPage from "../../notifications/notificationPage";
 
-    export default {
-        name: "desktop-header",
-        props:['user'],
-        data() {
-            return {
-                publicPath: process.env.VUE_APP_MAIN_API
-            }
-        },
-        methods: {
-            routerPush(linkName){
+	export default {
+		name: "desktopHeader",
+		props: ['user'],
+		data() {
+			return {
+				publicPath: process.env.VUE_APP_MAIN_API,
+				listScrollDifference: null
+			}
+		},
+		methods: {
 
-                this.$router.push({name:`${linkName}`})
+			setListScrollDifference(difference) {
+				this.listScrollDifference = difference;
+			},
 
-            },
-            userLink(userId){
+			routerPush(linkName) {
 
-                this.$router.push({name:'user',params:{id:userId}})
+				this.$router.push({name: `${linkName}`})
 
-            },
-        },
-        components: {
-            NotificationPage,
-            DropdownListReusable,
-            PictureReusable,
-            IconBase,
-            IconTextLogo,
-            IconLogo,
-            IconNotifications,
-            IconPocket,
-            IconDropdown}
-    }
+			},
+			userLink(userId) {
+
+				this.$router.push({name: 'user', params: {id: userId}})
+
+			},
+		},
+		components: {
+			NotificationPage,
+			DropdownListReusable,
+			PictureReusable,
+			IconBase,
+			IconTextLogo,
+			IconLogo,
+			IconNotifications,
+			IconPocket,
+			IconDropdown
+		}
+	}
 </script>
 
 <style lang="scss">
-    .desktop-header {
-        background: #FFFFFF;
-        width: 100%;
-        height: 60px;
-        border-bottom: 1px solid #BCBEC3;
+	.desktop-header {
+		background: #FFFFFF;
+		width: 100%;
+		height: 60px;
+		border-bottom: 1px solid #BCBEC3;
 
-        .nav-container {
-            height: 60px;
-            display: flex;
-            justify-content: space-between;
-        }
-        .search-block {
-            input {
-                padding-left: 15px;
-                border: 0.5px solid #CECFD3;
-                box-sizing: border-box;
-                border-radius: 6px;
-                height: 27px;
-            &::placeholder {
-                font-family: Roboto;
-                font-style: normal;
-                font-weight: normal;
-                font-size: 12px;
-                line-height: 14px;
-                display: flex;
-                align-items: center;
-                color: #76787A;
-                opacity: 0.6;
+		.nav-container {
+			height: 60px;
+			display: flex;
+			justify-content: space-between;
+		}
 
-            }
-            &:focus, active {
-                outline: none;
-            }
-            }
-        }
-    }
+		.search-block {
+			input {
+				padding-left: 15px;
+				border: 0.5px solid #CECFD3;
+				box-sizing: border-box;
+				border-radius: 6px;
+				height: 27px;
+
+				&::placeholder {
+					font-family: Roboto;
+					font-style: normal;
+					font-weight: normal;
+					font-size: 12px;
+					line-height: 14px;
+					display: flex;
+					align-items: center;
+					color: #76787A;
+					opacity: 0.6;
+
+				}
+
+				&:focus, active {
+					outline: none;
+				}
+			}
+		}
+
+		.profile-annotation-block {
+			.notification-list {
+				overflow-y: scroll;
+				scrollbar-width: thin;
+
+				&::-webkit-scrollbar-thumb {
+					height: 6px;
+					border: 4px solid transparent;
+					background-clip: padding-box;
+					-webkit-border-radius: 7px;
+					background-color: rgba(0,0,0,.15);
+					-webkit-box-shadow: inset -1px -1px 0 rgba(0,0,0,.05), inset 1px 1px 0 rgba(0,0,0,.05);
+				}
+
+				&::-webkit-scrollbar-corner {
+					background-color: transparent;
+				}
+
+				&::-webkit-scrollbar-button {
+					width: 0;
+					height: 0;
+					display: none;
+				}
+
+				&::-webkit-scrollbar {
+					width: 14px;
+					height: 18px;
+				}
+			}
+		}
+	}
 </style>
