4823948d3d51ebab66b72882d49337b832e1c6d3
Orest <k.orest@opinionsn.com>
2019-07-26 19:29:18 +0300

    Сделал загрузку аватарок

--- vue.config.js
+++ vue.config.js
@@ -24,21 +24,21 @@ module.exports = {
 			// secure: false
 			// }
 			'/api': {
-				target: 'http://eng.opinionsn.com:7070/',
+				target: 'http://192.168.1.38:7070/',
 				pathRewrite: {'^/api': '/'},
 				changeOrigin: true,
 				secure: false
 			},
 
 			'/messages': {
-				target: 'eng.opinionsn.com:7071/',
+				target: 'http://192.168.1.38:7071/',
 				pathRewrite: {'^/messages': ''},
 				changeOrigin: true,
 				secure: false
 			},
 
 			'/assets': {
-				target: 'http://192.168.1.39/assets/',
+				target: 'http://192.168.1.38/assets/',
 				// pathRewrite: {'^/messages': '/'},
 				changeOrigin: true,
 				secure: false
