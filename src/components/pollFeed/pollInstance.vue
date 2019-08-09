e6740add04ba9f4b95a25a92f08c8a025120fbd2
Konstantin Nikolaev <k.nikolaev@opinionsn.com>
2019-08-09 12:17:24 +0300

    == фиксы

--- src/components/pollFeed/pollInstance.vue
+++ src/components/pollFeed/pollInstance.vue
@@ -11,7 +11,7 @@
         <headline-body :poll="poll" :item="item"/>
         <options-section
 		        @click.native="options_visible = true"
-		        :optionsVisible="options_visible"
+		        :optionsVisible="(combinedOptions.length <= 5) || options_visible"
 				:access-check="pollAccessCheck"
 				v-for="(option, index) in combinedOptions"
 				v-show="options_visible || index < 5"
