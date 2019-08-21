58dd583522d688362083aca74a464130639925ca
orest <k.orest@opinionsn.com>
2019-08-21 10:15:03 +0300

    Переделываю модальное окно 2

--- src/components/reusableСomponents/reusableModal.vue
+++ src/components/reusableСomponents/reusableModal.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="modal-reusable" v-if="pictures.length">
         <div @click="onModalClick">
-            <slot >
+            <slot>
 
             </slot>
         </div>
-            <icon-base
-                    @click.native="clearPictures"
-                    class="icon-close pointer"
-                    fill="none"
-                    width="40"
-                    height="40"
-                    viewBox="0 0 30 30"
-                    icon-name="close">
-                <icon-close/>
-            </icon-base>
+        <icon-base
+                @click.native="clearPictures"
+                class="icon-close pointer"
+                fill="none"
+                width="40"
+                height="40"
+                viewBox="0 0 30 30"
+                icon-name="close">
+            <icon-close/>
+        </icon-base>
         <div class="modal-overlay">
 
         </div>
@@ -23,12 +23,11 @@
                 <div class="picture-description" v-if="description">
                     <span>{{description}}</span>
                 </div>
-                <img class="main-picture" :src="pictures[0]" />
-<!--                <div v-if="pictures.length >= 2" class="pictures-section flex ">-->
-<!--                    <img v-for="(picture, index) in pictures"  :key="index" :src="picture" :class="{borderedPicture : picture === pictures[0]}" @click="sortPicturesArray(key)"/>-->
-<!--                    <span class="white" style="{color:white}">{{pictures}}</span>-->
+                <img class="main-picture" :src="(sorted_pictures && sorted_pictures[0]) || pictures[0]"/>
+                <div v-if="pictures.length >= 2" class="pictures-section flex ">
+                    <img v-for="(picture, index) in sorted_pictures || pictures"  :key="index" :src="picture" :class="{borderedPicture : picture === pictures[0]}" @click="sortPicturesArray(index)"/>
 
-<!--                </div>-->
+                </div>
             </div>
 
         </div>
@@ -44,32 +43,34 @@
     export default {
         name: "reusableModal",
         components: {IconBase, PictureReusable, IconClose},
-        mixins:[ImageMixin],
+        mixins: [ImageMixin],
         data() {
             return {
                 publicPath: process.env.VUE_APP_MAIN_API,
+                sorted_pictures: null
             }
         },
         computed: {
 
-            pictures(){
+            pictures() {
                 return this.$popup.pictures.map(pic => this.publicPath + this.imageUtil(pic, 'L'));
             }
 
         },
         methods: {
-            sortPicturesArray(key) {
-                this.pictures = [...this.pictures.splice(key, 1), ...this.pictures];
+            sortPicturesArray(index) {
+                let arr = [...this.pictures];
+                this.sorted_pictures = [...arr.splice(index, 1), ...arr];
             },
-            clearPictures(){
-              this.$popup.clear('pictures')
-
+            clearPictures() {
+                this.sorted_pictures = null;
+                this.$popup.clear('pictures');
             },
-            onModalClick(){
+            onModalClick() {
                 this.$emit('show')
             },
 
-            onCloseAction(){
+            onCloseAction() {
                 this.$emit('close');
                 this.hideModal = false
             }
@@ -79,32 +80,38 @@
 
 <style lang="scss">
     .white {
-        color:white;
+        color: white;
     }
+
     .modal-reusable {
         position: relative;
+
         .modal-wrapper {
             position: fixed;
             z-index: 9999999;
             top: 0;
-            right:0;
+            right: 0;
             left: 0;
             bottom: 0;
             background: #000;
             width: 100%;
             height: 100%;
         }
+
         .icon-close {
             position: fixed;
-            top:10%;
+            top: 10%;
             right: 3%;
             z-index: 999999999999999;
+
             svg {
                 fill: aqua;
             }
         }
+
         .picture-description {
             padding: 15px;
+
             span {
                 font-family: Roboto;
                 font-style: normal;
@@ -115,21 +122,26 @@
 
 
         }
+
         .modal-window {
             display: flex;
             flex-direction: column;
             align-items: center;
+
             .borderedPicture {
                 border: 1px solid #FFFFFF;
             }
+
             .main-picture {
                 width: 50%;
             }
+
             .pictures-section {
                 img {
                     width: 250px;
                 }
             }
+
             position: fixed;
             top: 50%;
             left: 50%;
