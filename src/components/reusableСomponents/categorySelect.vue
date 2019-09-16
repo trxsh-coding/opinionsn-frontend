c43ecfbcbcd8bcc6292c1b638c44ff20b0a5b87b
Konstantin Nikolaev <k.nikolaev@opinionsn.com>
2019-09-16 17:07:23 +0300

    - categorySelect: default categories fix

--- src/components/reusableСomponents/categorySelect.vue
+++ src/components/reusableСomponents/categorySelect.vue
@@ -1,100 +1,93 @@
 <template>
-    <div class="category-block">
-        <ReSwiper :type="mobile ? 'scroll' : 'usual'"
-                  :params="{height: 38, width: '100%', slidesPerView, spaceBetween: 10, breakpoints: false}">
-            <template #usual>
-                <swiper-slide
-                        class="w-fit"
-                        v-for="category in combinedCategories">
-                    <catalog-item
-                            :item="category"
-                            :is-current-string="isCurrentString"
-                            :current="current"
-                            @click.native="onCategorySelect(category.id, category.name)" />
-                </swiper-slide>
-            </template>
-            <template #scroll>
-                <catalog-item
-                        v-for="category in combinedCategories"
-                        :item="category"
-                        :is-current-string="isCurrentString"
-                        :current="current"
-                        @click.native="onCategorySelect(category.id, category.name)" />
-            </template>
-        </ReSwiper>
-        
-    </div>
+	<div class="category-block">
+		<ReSwiper :type="mobile ? 'scroll' : 'usual'"
+		          :params="{height: 38, width: '100%', slidesPerView, spaceBetween: 10, breakpoints: false}">
+			<template #usual>
+				<swiper-slide
+						class="w-fit"
+						v-for="category in combinedCategories">
+					<catalog-item
+							:item="category"
+							:is-current-string="isCurrentString"
+							:current="current"
+							@click.native="onCategorySelect(category.id, category.name)"/>
+				</swiper-slide>
+			</template>
+			<template #scroll>
+				<catalog-item
+						v-for="category in combinedCategories"
+						:item="category"
+						:is-current-string="isCurrentString"
+						:current="current"
+						@click.native="onCategorySelect(category.id, category.name)"/>
+			</template>
+		</ReSwiper>
+	
+	</div>
 </template>
 
 <script>
-    import { mapState } from 'vuex'
-    import langMixin from '../mixins/langMixin'
-    import CatalogItem from "../CatalogFeed/catalogItem";
-    import ReSwiper from "./ReSwiper";
-    
-    export default {
-        name: "categorySelect",
-        components: {ReSwiper, CatalogItem},
-        mixins:[langMixin],
-        props:['current', 'isCurrentString', 'slidesPerView'],
-        data() {
-            return {
-                localCategory: {
-                    "18": {
-                        id:-2,
-                        name:'new',
-                        path_to_image:require('../../assets/new.svg'),
-                        local: true},
-                    "20":{
-                        name:'all',
-                        id:-1,
-                        path_to_image:require('../../assets/all.svg'),
-                        local: true
-                    },
-                },
-            }
-        },
-        computed: {
-
-            ...mapState('globalStore', {
-
-                categories: ({categories}) =>categories,
-
-            }),
-    
-            mobile() {
-                return this.$root.mobile;
-            },
-            
-            combinedCategories: function(){
-                let {localCategory, categories} = this;
-                const unordered ={...localCategory, ...categories}
-                const array = Object.values(unordered)
-                const ordered = array.reverse();
-                if(this.$route.name === 'pollFeed') {
-                    return ordered;
-
-                } else {
-                    return categories;
-                }
-            },
-        },
-        mounted(){
-            this.$store.dispatch(`catalogList/list`);
-
-        },
-        methods: {
-            onCategorySelect(id, name){
-                this.$emit('on-select', { id, name })
-            },
-        }
-    }
+	import {mapState} from 'vuex'
+	import langMixin from '../mixins/langMixin'
+	import CatalogItem from "../CatalogFeed/catalogItem";
+	import ReSwiper from "./ReSwiper";
+	
+	export default {
+		name: "categorySelect",
+		components: {ReSwiper, CatalogItem},
+		mixins: [langMixin],
+		props: ['current', 'isCurrentString', 'slidesPerView'],
+		data() {
+			return {
+				localCategory: {
+					"998": {
+						id: -2,
+						name: 'new',
+						path_to_image: require('../../assets/new.svg'),
+						local: true
+					},
+					"999": {
+						name: 'all',
+						id: -1,
+						path_to_image: require('../../assets/all.svg'),
+						local: true
+					},
+				},
+			}
+		},
+		computed: {
+			
+			...mapState('globalStore', {
+				
+				categories: ({categories}) => categories,
+				
+			}),
+			
+			mobile() {
+				return this.$root.mobile;
+			},
+			
+			combinedCategories: function () {
+				const ordered = Object.values({...this.localCategory, ...this.categories}).reverse();
+				return (this.$route.name === 'pollFeed') ? ordered : this.categories;
+			},
+		},
+		mounted() {
+			this.$store.dispatch(`catalogList/list`);
+			
+		},
+		methods: {
+			onCategorySelect(id, name) {
+				this.$emit('on-select', {id, name})
+			},
+		}
+	}
 </script>
 
 <style lang="scss">
-    .category-block {
-        .swiper-container {
-            height: 38px;
-        }
-    }
+	.category-block {
+		.swiper-container {
+			height: 38px;
+		}
+	}
 </style>
