d4713e51bfad1e61406c78ae9ef2b8e1b5be6f66
rzalegal <vsimanov@opinionsn.com>
2019-11-12 10:32:48 +0300

    @ oauth btns back

--- src/components/Login/Registration.vue
+++ src/components/Login/Registration.vue
@@ -106,61 +106,61 @@
 					bg-color="#8f000f"
 					color="#ffffff"/>
 
-<!--			<div class="oAuth-btns mt-25 flex-column-center">-->
+			<div class="oAuth-btns mt-25 flex-column-center">
 
 
-<!--				<span class="title">-->
-<!--					<lang-string title="already_registered?"/>-->
-<!--					<router-link class="title pointer underline ml-5" to="/sign">-->
-<!--						<lang-string title="sign"/>-->
-<!--					</router-link>-->
-<!--				</span>-->
+				<span class="title">
+					<lang-string title="already_registered?"/>
+					<router-link class="title pointer underline ml-5" to="/sign">
+						<lang-string title="sign"/>
+					</router-link>
+				</span>
 
-<!--				<lang-string class="title mt-26" title="sign_in_with_social_networks"/>-->
+				<lang-string class="title mt-26" title="sign_in_with_social_networks"/>
 
-<!--				<div class="btns flex-align-center mt-10">-->
+				<div class="btns flex-align-center mt-10">
 
-<!--					<a :href="`https://opinionsn.com/api/oauth2/vk${queryList}`">-->
-<!--						<button-reusable-->
-<!--								class="v-center soc-btn vk-btn py-12"-->
-<!--								font-size="16"-->
-<!--								bor-rad="6"-->
-<!--								bg-color="#4C6C91"-->
-<!--								color="#4B97B4">-->
-<!--							<icon-base-->
-<!--									class="logo"-->
-<!--									width="32"-->
-<!--									height="18"-->
-<!--									viewBox="0 0 32 18"-->
-<!--									fill="none"-->
-<!--									icon-name="google-logo">-->
-<!--								<icon-vk/>-->
-<!--							</icon-base>-->
-<!--						</button-reusable>-->
-<!--					</a>-->
+					<a :href="`https://opinionsn.com/api/oauth2/vk${queryList}`">
+						<button-reusable
+								class="v-center soc-btn vk-btn py-12"
+								font-size="16"
+								bor-rad="6"
+								bg-color="#4C6C91"
+								color="#4B97B4">
+							<icon-base
+									class="logo"
+									width="32"
+									height="18"
+									viewBox="0 0 32 18"
+									fill="none"
+									icon-name="google-logo">
+								<icon-vk/>
+							</icon-base>
+						</button-reusable>
+					</a>
 
-<!--					<a :href="`https://opinionsn.com/api/oauth2/google${queryList}`">-->
-<!--						<button-reusable-->
-<!--								class="v-center soc-btn google-btn py-8 ml-11"-->
-<!--								font-size="16"-->
-<!--								bor-rad="6"-->
-<!--								bg-color="#ffffff"-->
-<!--								color="#4B97B4">-->
-<!--							<icon-base-->
-<!--									class="logo"-->
-<!--									width="26"-->
-<!--									height="26"-->
-<!--									viewBox="0 0 366 372"-->
-<!--									fill="none"-->
-<!--									icon-name="google-logo">-->
-<!--								<icon-google/>-->
-<!--							</icon-base>-->
-<!--						</button-reusable>-->
-<!--					</a>-->
+					<a :href="`https://opinionsn.com/api/oauth2/google${queryList}`">
+						<button-reusable
+								class="v-center soc-btn google-btn py-8 ml-11"
+								font-size="16"
+								bor-rad="6"
+								bg-color="#ffffff"
+								color="#4B97B4">
+							<icon-base
+									class="logo"
+									width="26"
+									height="26"
+									viewBox="0 0 366 372"
+									fill="none"
+									icon-name="google-logo">
+								<icon-google/>
+							</icon-base>
+						</button-reusable>
+					</a>
 
-<!--				</div>-->
+				</div>
 
-<!--			</div>-->
+			</div>
 
 		</div>
 	</div>
@@ -274,20 +274,20 @@
 							// 	this.$router.push({name: "pollFeed"});
 							// }
 
-							if (status === 200) {
-								this.$store.commit(
-									"authentication/setAuthenticated",
-									true
-								);
-								if (this.$route.query.redirectToPoll) {
-									this.$router.push({
-										name: "singlePoll",
-										params: {id: this.$route.query.redirectToPoll}
-									});
-								} else {
-									this.$router.push({path: "/"});
-								}
-							}
+							// if (status === 200) {
+							// 	this.$store.commit(
+							// 		"authentication/setAuthenticated",
+							// 		true
+							// 	);
+							// 	if (this.$route.query.redirectToPoll) {
+							// 		this.$router.push({
+							// 			name: "singlePoll",
+							// 			params: {id: this.$route.query.redirectToPoll}
+							// 		});
+							// 	} else {
+							// 		this.$router.push({path: "/"});
+							// 	}
+							// }
 						} catch (e) {
 							console.error('Error sending sign form: ', e);
 						}
