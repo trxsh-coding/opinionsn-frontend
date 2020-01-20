0815674ddd6520d1d01d65103b56341e373d4f16
rzalegal <vsimanov@opinionsn.com>
2020-01-20 17:05:45 +0300

    @ FB!

--- src/store/modules/adminPage.js
+++ src/store/modules/adminPage.js
@@ -2,10 +2,7 @@ import {VuexStore} from "./generic/proto";
 import {AdminStoreWithPageList} from "./generic/adminPageList";
 import {adminActions} from "./adminActions";
 
-class adminPageStore extends adminActions(AdminStoreWithPageList (VuexStore, `${process.env.VUE_APP_MAIN_API}/rest/admin/panel/prediction`)) {
-
-
-
+class adminPageStore extends adminActions(AdminStoreWithPageList (VuexStore, `${process.env.VUE_APP_MAIN_API}/rest/v1/admin/panel/prediction/page`)) {
 
 
 }
