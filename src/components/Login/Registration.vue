d3c8bdd8f26b855f06de83d3325043636b6d3cdf
faraks01 <k.nikolaev@opinionsn.com>
2020-03-23 10:05:40 +0300

    -- fixed register loading boolean

--- src/components/Login/Registration.vue
+++ src/components/Login/Registration.vue
@@ -1,112 +1,112 @@
 <template lang="html">
-    <div class="register-section">
-        <div class="navbar-brand">
-            <div class="navbar__item navbar__item-1">
+	<div class="register-section">
+		<div class="navbar-brand">
+			<div class="navbar__item navbar__item-1">
 
-                <icon-base
-                        fill="none"
-                        class="icon"
-                        width="75"
-                        height="64"
-                        viewBox="0 0 23 24"
-                        icon-name="logo">
-                    <icon-logo/>
-                </icon-base>
-            </div>
-            <div class="navbar__item navbar__item-2 v-center">
-                <icon-base
-                        fill="none"
-                        class="icon-text"
-                        width="66"
-                        height="15"
-                        viewBox="0 0 64 15"
-                        icon-name="text-logo">
-                    <icon-text-logo/>
-                </icon-base>
-            </div>
-        </div>
+				<icon-base
+						fill="none"
+						class="icon"
+						width="75"
+						height="64"
+						viewBox="0 0 23 24"
+						icon-name="logo">
+					<icon-logo/>
+				</icon-base>
+			</div>
+			<div class="navbar__item navbar__item-2 v-center">
+				<icon-base
+						fill="none"
+						class="icon-text"
+						width="66"
+						height="15"
+						viewBox="0 0 64 15"
+						icon-name="text-logo">
+					<icon-text-logo/>
+				</icon-base>
+			</div>
+		</div>
 
-        <div class="form-block mt-25" @keyup.enter.exact="submit(registrationForm)">
+		<div class="form-block mt-25" @keyup.enter.exact="submit(registrationForm)">
 
-            <popup-error-reusable :errors="{ login: lstr(errors.login) }" span-class="mt-3">
-                <input-reusable class="mx-auto"
-                                :value="registrationForm.username"
-                                @change="updateField(arguments[0], 'username')"
-                                inputPlaceholder="login"
-                                width="271"
-                                input
-                                with-underline/>
-            </popup-error-reusable>
+			<popup-error-reusable :errors="{ login: lstr(errors.login) }" span-class="mt-3">
+				<input-reusable class="mx-auto"
+				                :value="registrationForm.username"
+				                @change="updateField(arguments[0], 'username')"
+				                inputPlaceholder="login"
+				                width="271"
+				                input
+				                with-underline/>
+			</popup-error-reusable>
 
 
-            <popup-error-reusable :errors="{email: lstr(errors.email) }" span-class="mt-3">
-                <input-reusable class="mx-auto mt-16"
-                                :value="registrationForm.email"
-                                @change="updateField(arguments[0], 'email')"
-                                inputPlaceholder="email"
-                                width="271"
-                                input
-                                with-underline/>
-            </popup-error-reusable>
+			<popup-error-reusable :errors="{email: lstr(errors.email) }" span-class="mt-3">
+				<input-reusable class="mx-auto mt-16"
+				                :value="registrationForm.email"
+				                @change="updateField(arguments[0], 'email')"
+				                inputPlaceholder="email"
+				                width="271"
+				                input
+				                with-underline/>
+			</popup-error-reusable>
 
-            <popup-error-reusable :errors="{passConfirm: lstr(errors.pass) }" span-class="mt-3">
-                <input-reusable class="mx-auto mt-16"
-                                input-type="password"
-                                :value="registrationForm.password"
-                                @change="updateField(arguments[0], 'password')"
-                                inputPlaceholder="password"
-                                width="271"
-                                input
-                                with-underline/>
-            </popup-error-reusable>
+			<popup-error-reusable :errors="{passConfirm: lstr(errors.pass) }" span-class="mt-3">
+				<input-reusable class="mx-auto mt-16"
+				                input-type="password"
+				                :value="registrationForm.password"
+				                @change="updateField(arguments[0], 'password')"
+				                inputPlaceholder="password"
+				                width="271"
+				                input
+				                with-underline/>
+			</popup-error-reusable>
 
-            <popup-error-reusable :errors="{passConfirm: lstr(errors.passConfirm) }" span-class="mt-3">
-                <input-reusable class="mx-auto mt-16"
-                                input-type="password"
-                                :value="registrationForm.conf_pass"
-                                @change="updateField(arguments[0], 'conf_pass')"
-                                inputPlaceholder="confirm_password"
-                                width="271"
-                                input
-                                with-underline/>
-            </popup-error-reusable>
+			<popup-error-reusable :errors="{passConfirm: lstr(errors.passConfirm) }" span-class="mt-3">
+				<input-reusable class="mx-auto mt-16"
+				                input-type="password"
+				                :value="registrationForm.conf_pass"
+				                @change="updateField(arguments[0], 'conf_pass')"
+				                inputPlaceholder="confirm_password"
+				                width="271"
+				                input
+				                with-underline/>
+			</popup-error-reusable>
 
-        </div>
+		</div>
 
-        <!--		<popup-error-reusable :errors="{passConfirm: lstr(errors.passConfirm) }" span-class="mt-3">-->
+		<!--		<popup-error-reusable :errors="{passConfirm: lstr(errors.passConfirm) }" span-class="mt-3">-->
 
-        <!--			<vue-recaptcha-->
-        <!--					class="recaptcha mt-20"-->
-        <!--					ref="recaptcha"-->
-        <!--					:sitekey="sitekey"-->
-        <!--					@verify="setCurrentTOken(arguments[0])"-->
-        <!--					@expired="onCaptchaExpired"-->
-        <!--					:loadRecaptchaScript="true"-->
-        <!--			/>-->
-        <!--		</popup-error-reusable>-->
+		<!--			<vue-recaptcha-->
+		<!--					class="recaptcha mt-20"-->
+		<!--					ref="recaptcha"-->
+		<!--					:sitekey="sitekey"-->
+		<!--					@verify="setCurrentTOken(arguments[0])"-->
+		<!--					@expired="onCaptchaExpired"-->
+		<!--					:loadRecaptchaScript="true"-->
+		<!--			/>-->
+		<!--		</popup-error-reusable>-->
 
-        <div class="buttons-block mt-23">
+		<div class="buttons-block mt-23">
 
-            <button-reusable
-                    @click.native="submit(registrationForm)"
-                    class="v-center reg-btn py-13"
-                    description="registration"
-                    font-size="16"
-                    bor-rad="6"
-                    bg-color="#4B97B4"
-                    color="#ffffff"/>
+			<button-reusable
+					@click.native="loading ? check() : submit(registrationForm)"
+					class="v-center reg-btn py-13"
+					description="registration"
+					font-size="16"
+					bor-rad="6"
+					bg-color="#4B97B4"
+					color="#ffffff"/>
 
-            <!--			<button-reusable-->
-            <!--					v-if="$root.is_eng"-->
-            <!--					@click.native="submitAuto()"-->
-            <!--					class="v-center reg-btn py-13 mt-9"-->
-            <!--					description="AUTO"-->
-            <!--					font-size="16"-->
-            <!--					bor-rad="6"-->
-            <!--					bg-color="#8f000f"-->
-            <!--					color="#ffffff"/>-->
+<!--			<button-reusable-->
+<!--					v-if="$root.is_eng"-->
+<!--					@click.native="submitAuto()"-->
+<!--					class="v-center reg-btn py-13 mt-9"-->
+<!--					description="AUTO"-->
+<!--					font-size="16"-->
+<!--					bor-rad="6"-->
+<!--					bg-color="#8f000f"-->
+<!--					color="#ffffff"/>-->
 
-            <div class="oAuth-btns mt-25 flex-column-center">
+			<div class="oAuth-btns mt-25 flex-column-center">
 
 
 				<span class="title">
@@ -116,49 +116,50 @@
 					</router-link>
 				</span>
 
-                <lang-string class="title mt-26" title="sign_in_with_social_networks"/>
+				<lang-string class="title mt-26" title="sign_in_with_social_networks"/>
 
-                <div class="btns flex-align-center mt-10">
+				<div class="btns flex-align-center mt-10">
 
-                    <button-reusable
-							@click.native="_ => oAuth2VkontakteSignIn(queryList)"
-                            class="v-center soc-btn vk-btn py-12"
-                            font-size="16"
-                            bor-rad="6"
-                            bg-color="#4C6C91"
-                            color="#4B97B4">
-                        <icon-base
-                                class="logo"
-                                width="32"
-                                height="18"
-                                viewBox="0 0 32 18"
-                                fill="none"
-                                icon-name="google-logo">
-                            <icon-vk/>
-                        </icon-base>
-                    </button-reusable>
+					<a :href="`https://opinionsn.com/api/oauth2/vk${queryList}`">
+						<button-reusable
+								class="v-center soc-btn vk-btn py-12"
+								font-size="16"
+								bor-rad="6"
+								bg-color="#4C6C91"
+								color="#4B97B4">
+							<icon-base
+									class="logo"
+									width="32"
+									height="18"
+									viewBox="0 0 32 18"
+									fill="none"
+									icon-name="google-logo">
+								<icon-vk/>
+							</icon-base>
+						</button-reusable>
+					</a>
 
-                    <!--					<a :href="`https://opinionsn.com/api/oauth2/facebook${queryList}`">-->
-                    <!--						<button-reusable-->
-                    <!--								class="v-center soc-btn facebook-btn py-8 ml-11"-->
-                    <!--								font-size="16"-->
-                    <!--								bor-rad="6"-->
-                    <!--								bg-color="#3b5998"-->
-                    <!--								color="#4B97B4">-->
-                    <!--							<IconFacebook />-->
-                    <!--						</button-reusable>-->
-                    <!--					</a>-->
+<!--					<a :href="`https://opinionsn.com/api/oauth2/facebook${queryList}`">-->
+<!--						<button-reusable-->
+<!--								class="v-center soc-btn facebook-btn py-8 ml-11"-->
+<!--								font-size="16"-->
+<!--								bor-rad="6"-->
+<!--								bg-color="#3b5998"-->
+<!--								color="#4B97B4">-->
+<!--							<IconFacebook />-->
+<!--						</button-reusable>-->
+<!--					</a>-->
 
-                    <!--					<a @click="authWithFB">-->
-                    <!--						<button-reusable-->
-                    <!--								class="v-center soc-btn facebook-btn py-8 ml-11"-->
-                    <!--								font-size="16"-->
-                    <!--								bor-rad="6"-->
-                    <!--								bg-color="#3b5998"-->
-                    <!--								color="#4B97B4">-->
-                    <!--							<IconFacebook />-->
-                    <!--						</button-reusable>-->
-                    <!--					</a>-->
+<!--					<a @click="authWithFB">-->
+<!--						<button-reusable-->
+<!--								class="v-center soc-btn facebook-btn py-8 ml-11"-->
+<!--								font-size="16"-->
+<!--								bor-rad="6"-->
+<!--								bg-color="#3b5998"-->
+<!--								color="#4B97B4">-->
+<!--							<IconFacebook />-->
+<!--						</button-reusable>-->
+<!--					</a>-->
 
                     <button-reusable
                             @click.native="_ => oAuth2GoogleSignIn(queryList)"
@@ -210,6 +211,7 @@
 			return {
 				sitekey: '6Ld7BbcUAAAAAMRiV7C5mb0Co0KUpKKau6f3jky6',
 				token: null,
+				loading:false,
 				registrationForm: {
 					username: '',
 					email: '',
@@ -257,7 +259,10 @@
 				"oAuth2GoogleSignIn"
 			]),
 
-			authWithFB() {
+			check() {
+                console.log(this.loading);
+            },
+			authWithFB(){
 
 				window.sessionStorage.setItem('queryList', this.queryList);
 				if (window.FB) {
@@ -285,7 +290,7 @@
 				this.$refs.recaptcha.execute();
 			},
 			async submit(form) {
-
+				this.loading = true
 				try {
 					let registerFormData = new FormData();
 					registerFormData.append("login", form.username);
@@ -335,6 +340,8 @@
 							// }
 						} catch (e) {
 							console.error('Error sending sign form: ', e);
+						} finally {
+
 						}
 
 					}
@@ -351,6 +358,8 @@
 					}
 
 					this.$forceUpdate();
+				} finally {
+					this.loading = false
 				}
 
 			},
--- src/store/modules/Login.js
+++ src/store/modules/Login.js
@@ -68,7 +68,8 @@ export default  {
     password:'',
     rules: {
 
-    }
+    },
+    loading:false
   },
   getters: {
   },
